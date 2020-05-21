3d1d511d0e2fd1308c4db9a5cf48328f016ff21b
Chris Ball <chrisball02@gmail.com>
2020-05-21 16:05:48 +0200

    Change name in readme and index

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Inceptionizer
+# Inceptionize
+
 An application that helps teams and individuals organize inceptions in the easiest way possible by providing first steps and necessary resources to get them started
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 
 function HomePage(): ReactElement {
-  return <div>Inceptionizer! Coming Soon...</div>;
+  return <div>Inceptionize! Coming Soon...</div>;
 }
 
 export default HomePage;
