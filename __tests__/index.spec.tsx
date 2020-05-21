706b148f8df96b7840636f4aad6f8e1196983fd1
Chris Ball <chrisball02@gmail.com>
2020-05-21 13:12:16 +0200

    Move __tests__ outside of pages as only server side code goes here

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import HomePage from "../index";
+import HomePage from "../pages/index";
 
 describe("HomePage", () => {
   afterEach(cleanup);
