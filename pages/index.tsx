70bfd02b4e6daa0d3581fce2354d90614071e3d8
Chris Ball <chrisball02@gmail.com>
2020-05-22 17:12:42 +0200

    Change text to test deploy works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 
 function HomePage(): ReactElement {
-  return <div>Inceptionize Application! Coming Soon...</div>;
+  return <h1>Inceptionize! Coming Soon...</h1>;
 }
 
 export default HomePage;
