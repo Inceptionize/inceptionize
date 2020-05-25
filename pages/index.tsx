fd5a423e2052645fb849a9e915edd08c637c4842
Barbara Kowalewska <baw.kowalewska@gmail.com>
2020-05-25 16:13:09 +0200

    Add head component to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import React, { ReactElement } from "react";
+import Head from "next/head";
 
 function HomePage(): ReactElement {
-  return <h1>Inceptionize! Coming Soon...</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inceptionize</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Inceptionize! Coming Soon...</h1>
+    </>
+  );
 }
 
 export default HomePage;
