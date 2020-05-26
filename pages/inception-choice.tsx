08ceb599e07fcd262212da56fe9f59c1db35725a
Chris Ball <chrisball02@gmail.com>
2020-05-26 15:48:19 +0200

    Keep file names consistent

--- pages/inception-choice.tsx
+++ pages/inception-choice.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import Layout from "../components/Layout";
-import DecisionButton from "../components/decision-button";
+import DecisionButton from "../components/DecisionButton";
 
 function InceptionChoice(): ReactElement {
   return (
