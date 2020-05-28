ac23b1e70341b7670a08beba3eca340e319dce76
Barbara Kowalewska <baw.kowalewska@gmail.com>
2020-05-28 16:09:22 +0200

    #14 Add links to lean inception page

---
+++ components/lean-inception-content/index.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from "react";
+import styles from "./leaninceptioncontent.module.scss";
+
+function LeanInceptionContent(): ReactElement {
+  return (
+    <div className={styles.content}>
+      <p>
+        Find out more about Lean Inception:
+        <a href="https://www.caroli.org/en/book/lean-inception-how-to-align-people-and-build-the-right-product/">
+          caroli.org
+        </a>
+        <a href="https://www.slideshare.net/paulocaroli/lean-inception-how-to-align-people-and-build-the-right-product">
+          How to align people and build the right product
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default LeanInceptionContent;
--- pages/lean-inception.tsx
+++ pages/lean-inception.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from "react";
 import Layout from "../components/layout";
+import LeanInceptionContent from "../components/lean-inception-content";
 
 function LeanInception(): ReactElement {
   return (
     <Layout>
-      <h2>Lean Inception Page</h2>
+      <h2>Lean Inception</h2>
+      <LeanInceptionContent />
     </Layout>
   );
 }
