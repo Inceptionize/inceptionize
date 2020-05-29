61b663a53642554333b4c72e17893833e8396d4b
Chris Ball <chrisball02@gmail.com>
2020-05-29 10:35:18 +0200

    Add custom document page to allow for overriding/customizing

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from "react";
+import Document, { DocumentContext, DocumentInitialProps, Html, NextScript, Main, Head } from "next/document";
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
