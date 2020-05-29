411b02ecae81909b119f10e5469ea7790b16d035
Chris Ball <chrisball02@gmail.com>
2020-05-29 11:24:28 +0200

    #14 Fix heading in agile team page

--- components/agile-team-content/index.tsx
+++ components/agile-team-content/index.tsx
@@ -4,7 +4,7 @@ import styles from "./agileteamcontent.module.scss";
 function AgileTeamContent(): ReactElement {
   return (
     <div className={styles.content}>
-      <h2>Agile Team</h2>
+      <h2>Agile Team Setup</h2>
       <p>Find out more about setting up an agile team:</p>
       <a href="https://www.funretrospectives.com/defining-the-team-principles/">agile team setup</a>
     </div>
