ac8d58c436ed065708b5c914a49e1e7bc39ab3ae
Barbara Kowalewska <baw.kowalewska@gmail.com>
2020-06-02 11:43:33 +0200

    #16 Create Remote Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import { AppProps } from "next/app";
 import "../styles/global.scss";
+import RemoteContext from "../components/remote/remotecontext";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  const [isRemote, setRemoteness] = useState(false);
+
+  return (
+    <RemoteContext.Provider value={{ isRemote, setRemoteness }}>
+      <Component {...pageProps} />;
+    </RemoteContext.Provider>
+  );
 }
