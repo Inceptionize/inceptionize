9e75866aed1fa3a1e693eed087935f81ba0c04b2
EmilyK <ekar45@gmail.com>
2020-06-03 15:04:38 +0200

    #16 Rename button props

--- components/decision-button/index.tsx
+++ components/decision-button/index.tsx
@@ -2,13 +2,13 @@ import React, { ReactElement } from "react";
 import DecisionLink from "../decision-link";
 import styles from "./decisionbutton.module.scss";
 
-interface LinkProps {
+interface ButtonProps {
   href: string;
   text: string;
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-function DecisionButton({ href, text, onClick }: LinkProps): ReactElement {
+function DecisionButton({ href, text, onClick }: ButtonProps): ReactElement {
   return (
     <button className={styles.button} onClick={onClick}>
       <DecisionLink href={href} text={text} />
