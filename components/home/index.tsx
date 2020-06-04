e4402b2e40408823b35fc937923f274fa55e0880
Arne Laponin <arne.laponin@thoughtworks.com>
2020-06-04 12:28:23 +0200

    Add styling

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,13 +1,10 @@
 import React, { ReactElement } from "react";
 import DecisionLink from "../decision-link";
-import styles from "./home.module.scss";
 
 function Home(): ReactElement {
   return (
     <div>
-      <h2 className={styles.subheading}>
-        Helping teams and individuals organize inceptions in the easiest way possible.
-      </h2>
+      <h2>Helping teams and individuals organize inceptions in the easiest way possible.</h2>
       <DecisionLink href="/remote" text="Get started" />
     </div>
   );
