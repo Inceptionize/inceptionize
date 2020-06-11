58fbe01392b96d7464ece727cb8cdfe63023738f
EmilyK <ekar45@gmail.com>
2020-06-11 10:57:36 +0200

    Num32: Add more detailed description and make styling responsive

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -13,6 +13,11 @@ function Home(): ReactElement {
           </span>
         </h2>
         <h2>Inceptionize! will help you and your team organize inceptions in the easiest way possible.</h2>
+        <h3>
+          Select inception activities, create agendas, prepare logistics, and learn about various types of inceptions
+          you can organize.
+        </h3>
+
         <DecisionLink href="/remote" text="Let's get started!" />
       </div>
       <div className={styles.column}>
