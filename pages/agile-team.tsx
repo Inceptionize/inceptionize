e16c973ff8489ae884276cc40ad16caca6613cf2
Arne Laponin <arne.laponin@thoughtworks.com>
2020-06-15 12:03:58 +0200

    Num38: Add GA to agile team page

--- pages/agile-team.tsx
+++ pages/agile-team.tsx
@@ -1,8 +1,23 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import Layout from "../components/layout";
 import AgileTeamContent from "../components/agile-team-content";
+import { initGA, logPageView } from "../components/utils/google-analytics";
+
+declare global {
+  interface Window {
+    GA_INITIALIZED: boolean;
+  }
+}
 
 function AgileTeam(): ReactElement {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  });
+
   return (
     <Layout>
       <AgileTeamContent />
