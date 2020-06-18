1dbda4c214983130da3b045dc6a6ca7204a34f0d
Arne Laponin <arne.laponin@thoughtworks.com>
2020-06-18 16:48:46 +0200

    Remove unnecessary GA init from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement } from "react";
 import { AppProps } from "next/app";
 import "../styles/global.scss";
 import { AppContextProvider } from "../components/context/app-context";
-import ReactGA from "react-ga";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  const App_ID = "UA-169236210-1";
-  useEffect(() => {
-    ReactGA.initialize(App_ID);
-  });
   return (
     <AppContextProvider>
       <Component {...pageProps} />;
