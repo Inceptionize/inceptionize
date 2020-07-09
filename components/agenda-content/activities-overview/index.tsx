c85dc022b2e53fd05600a5039a0d827d15b54f3d
annamts <>
2020-07-09 12:23:42 +0200

    Num.41 - Arne/Anna - Add cards styling to activities in agenda

--- components/agenda-content/activities-overview/index.tsx
+++ components/agenda-content/activities-overview/index.tsx
@@ -1,24 +1,32 @@
 import React, { ReactElement } from "react";
+import styles from "./activities-overview.module.scss";
 
 function ActivitiesOverview(): ReactElement {
   return (
-    <div data-testid="activities-overview">
-      <ul>
-        <li>
-          Kick-off - The kickoff session serves to guide the participants with the business reasons and its relationship
-          with the main objectives of the project and product. This initial communication allows all participants to
-          have an overview and a common understanding of what should be achieved. After the conversation about the
-          initiative, the inception facilitator will share the inception agenda.
-        </li>
-        <li>
-          Product Vision - Understanding the need of the product will lead the following activities. All starts with a
-          common vision, written together, by all team members.
-        </li>
-        <li>
-          Product Objectives (Is – Is not) - Each active team member will share what they understand about the product
-          goals. This should be discussed in order for the team to reach a consensus on what is really important.
-        </li>
-      </ul>
+    <div data-testid="activities-overview" className={styles.cards}>
+      <div className={styles.card}>
+        <h3>Kick-off</h3>
+        <p>
+          The kickoff session serves to guide the participants with the business reasons and its relationship with the
+          main objectives of the project and product. This initial communication allows all participants to have have an
+          overview and a common understanding of what should be achieved. After the conversation about the initiative,
+          the inception facilitator will share the inception agenda.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <h3>Product Vision</h3>
+        <p>
+          Understanding the need of the product will lead the following activities. All starts with a common vision,
+          written together, by all team members.
+        </p>
+      </div>
+      <div className={styles.card}>
+        <h3>Product Objectives</h3>
+        <p>
+          Each active team member will share what they understand about the product goals. This should be discussed in
+          order for the team to reach a consensus on what is really important.
+        </p>
+      </div>
     </div>
   );
 }
