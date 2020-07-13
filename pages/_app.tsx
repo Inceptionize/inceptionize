726a111aeb6eeb1aa3ed6cd1d21bcd7398e191a5
Arne Laponin <arne.laponin@thoughtworks.com>
2020-07-13 16:26:51 +0200

    Num42: Change styling of the calendar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement } from "react";
 import { AppProps } from "next/app";
+import "react-big-calendar/lib/sass/styles.scss";
 import "../styles/global.scss";
 import { AppContextProvider } from "../components/context/app-context";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
