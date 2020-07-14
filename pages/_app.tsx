1fd6d8cc9e0d99229b799c39dc73e3a0b0c1460d
Arne Laponin <arne.laponin@thoughtworks.com>
2020-07-14 13:43:16 +0200

    Num45: Remove trailing semicolon from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { AppContextProvider } from "../components/context/app-context";
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <AppContextProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AppContextProvider>
   );
 }
