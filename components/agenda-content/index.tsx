03338035632711bfce10b32a785315b38fce6aa5
Arne Laponin <arne.laponin@thoughtworks.com>
2020-07-15 16:35:55 +0200

    [#42] Remove feature toggles from the agenda page

--- components/agenda-content/index.tsx
+++ components/agenda-content/index.tsx
@@ -1,25 +1,16 @@
-import React, { ReactElement, useContext } from "react";
+import React, { ReactElement } from "react";
 import styles from "./agendacontent.module.scss";
 import PreviousStep from "../previous-step";
-import { AppContext } from "../context/app-context";
-import { selectActivitiesVisible } from "../../state/selector";
 import ActivitiesOverview from "./activities-overview";
 import ActivitiesCalendar from "./activities-calendar";
 
 function AgendaContent(): ReactElement {
-  const { state } = useContext(AppContext);
-  const isActivitiesVisible = selectActivitiesVisible(state);
-
   return (
     <div className={styles.content}>
-      <h1>The agenda creation is coming soon.</h1>
-      <div className={styles.agendaimage}>
-        <img src="/lean-inception-agenda.png" alt="Lean Inception Agenda" />
-      </div>
+      <h2>Agenda for the Lean Inception</h2>
 
-      {isActivitiesVisible ? <ActivitiesCalendar /> : null}
-
-      {isActivitiesVisible ? <ActivitiesOverview /> : null}
+      <ActivitiesCalendar />
+      <ActivitiesOverview />
 
       <PreviousStep href="/lean-inception" />
     </div>
