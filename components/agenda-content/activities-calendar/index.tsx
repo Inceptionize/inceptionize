1a32c6e0c730a4a9ecbc525a7050b06108fc44af
annamts <>
2020-07-16 12:37:49 +0200

    [#47] Change start and end of day, only show workweek and adjust height

--- components/agenda-content/activities-calendar/index.tsx
+++ components/agenda-content/activities-calendar/index.tsx
@@ -8,16 +8,22 @@ import dateHelpers from "../date-helpers/date-helpers";
 const localizer = momentLocalizer(moment);
 
 function ActivitiesCalendar(): ReactElement {
+  const todayStartOfDay: Date = new Date();
+  todayStartOfDay.setHours(8, 0, 0, 0);
+  const todayEndOfDay: Date = new Date();
+  todayEndOfDay.setHours(18, 0, 0, 0);
   return (
     <div className={styles.container}>
       <Calendar
         localizer={localizer}
         events={events.map((x) => dateHelpers.convertEvent(dateHelpers.getNextMonday(moment()), x))}
-        defaultView={"week"}
-        views={{ month: true, week: true, agenda: true }}
+        defaultView={"work_week"}
+        views={{ month: true, work_week: true, agenda: true }}
         startAccessor="start"
         endAccessor="end"
         defaultDate={dateHelpers.getNextMonday(moment()).toDate()}
+        min={todayStartOfDay}
+        max={todayEndOfDay}
       />
     </div>
   );
