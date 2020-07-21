a16c0b7723f49a8f9fdfeb9d65f83ea18f6b1f2e
annamts <>
2020-07-21 11:58:33 +0200

    [#49] Add modal

--- components/lean-inception-content/activities-overview/activity-card/index.tsx
+++ components/lean-inception-content/activities-overview/activity-card/index.tsx
@@ -2,6 +2,7 @@
 import React, { ReactElement } from "react";
 import styles from "../activities-overview.module.scss";
 import ReactTooltip from "react-tooltip";
+import Modal from "react-modal";
 
 interface ActivityCardProps {
   title: string;
@@ -9,9 +10,39 @@ interface ActivityCardProps {
 }
 
 function ActivityCard({ title, description }: ActivityCardProps): ReactElement {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div className={styles.card}>
-      <h3>{title}</h3>
+      <button onClick={openModal} className={styles.activityCardTitle}>
+        <h3>{title}</h3>
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
       <a data-tip={description} data-event="click focus">
         i
       </a>
